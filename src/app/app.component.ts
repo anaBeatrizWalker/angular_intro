069493ff3e2927e1c695bdca8f11065685dbc694
anaBeatrizWalker <95242519+anaBeatrizWalker@users.noreply.github.com>
2022-09-02 20:40:39 -0300

    Interpolação: Contador e Timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,22 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent {
   name = 'Ana Beatriz';
   nascimento = 2001;
+  counter = 0;
+  //Implementando counter sem atribui-lo para a pag html
+  timer = setInterval(() => {
+    this.counter = this.counter + 1;
+  }, 100);
 
-  //Método
+  //Métodos
   getIdade() {
     return 2022 - this.nascimento;
+  }
+
+  getMinutos() {
+    return Math.trunc(this.counter / 60); //pega só a parte inteira
+  }
+
+  getSegundos() {
+    return Math.trunc(this.counter % 60); //pega o resto da divisão, o que sobra dos minutos
   }
 }
