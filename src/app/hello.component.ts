beae4127c2658e65cb5a775b670e970115254ef6
anaBeatrizWalker <95242519+anaBeatrizWalker@users.noreply.github.com>
2022-09-02 20:20:28 -0300

    Testando bootstrap

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'hello',
   template: `<h1>Ana Beatriz - {{name}}</h1>`,
-  styles: [`h1 { font-family: Lato; }`],
+  styles: [`h1 { font-family: Lato;}`],
 })
 export class HelloComponent {
   @Input() name: string;
